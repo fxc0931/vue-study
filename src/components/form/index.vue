94b51246af5661dfbb9c7fff212b9b86b7cb50bb
fxc0931 <2568681147@qq.com>
2020-05-11 13:07:42 +0800

    feat: notice Component

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -18,6 +18,10 @@
 import XForm from "./Form";
 import XFormItem from "./FormItem";
 import XInput from "./Input";
+
+import create from '@/utils/create'
+import Notice from '@/components/notice'
+
 export default {
   components: {
     XForm,
@@ -41,7 +45,12 @@ export default {
     submitForm(form) {
       this.$refs[form].validate(valid => {
         if (valid) {
-          console.log("正确");
+          const notice = create(Notice, {
+            title: '正确',
+            message: '很正确aaaaaaaaa',
+            duration: 2000
+          })
+          notice.show()
         } else {
           console.log("错误");
         }
