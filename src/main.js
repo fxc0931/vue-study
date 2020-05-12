6922329064876ae979bf02ebdd9b8d325159bafc
fxc0931 <2568681147@qq.com>
2020-05-12 20:16:16 +0800

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // import router from './router'
 import router from './xrouter'
+
+// import store from './store'
+import store from './xstore'
 
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     Home
+    <div>冲啊，手榴弹扔了{{$store.state.count}}</div>
+    <p>{{$store.getters.score}}</p>
+    <button @click="add">扔一个</button>
+    <button @click="addAsync">蓄力仍两个</button>
   </div>
 </template>
 
@@ -9,6 +13,15 @@
 
 export default {
   name: 'Home',
+  methods: {
+      add() {
+          // 修改状态用commit
+          this.$store.commit("increment")
+      },
+      addAsync() {
+          this.$store.dispatch("incrementAsync")
+      }
+  }
 
 }
 </script>
